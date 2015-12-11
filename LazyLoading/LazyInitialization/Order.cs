f2825e08b5a5e27accf441a941f66453702da5b2
pongracz-lajos <pongraczlajoslevente@gmail.com>
2015-12-11 17:26:22 +0200

    Lazy init and virtual proxy implemented.

---
+++ LazyLoading/LazyInitialization/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoading.LazyInitialization
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+
+        public Order(int orderId)
+        {
+            OrderID = orderId;
+        }
+    }
+}
---
+++ LazyLoading/Program.cs
@@ -0,0 +1,57 @@
+﻿using LazyLoading.LazyInitialization;
+using LazyLoading.VirtualProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LazyLoading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var isRunning = true;
+
+            do
+            {
+                Console.WriteLine("A: Lazy Initialization.");
+                Console.WriteLine("B: Lazy Initialization native.");
+                Console.WriteLine("C: Virtual Proxy.");
+                Console.WriteLine("D: Value Holder.");
+                Console.WriteLine("E: Ghost.");
+                Console.WriteLine("Q: quit.");
+
+                Console.Write("Enter option: ");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.KeyChar)
+                {
+                    case 'a':
+                        Customer customer = new Customer(1, "Martin");
+
+                        foreach (Order order in customer.Orders)
+                        {
+                            Console.WriteLine("Got order {0} for customer {1}.", order.OrderID, customer.Name);
+                        }
+                        break;
+                    case 'b':
+                        break;
+                    case 'c':
+                        Image image = new VirtualProxyImage("teddy-bear.jpg");
+                        image.Display();
+                        break;
+                    case 'd':
+                        break;
+                    case 'q':
+                        isRunning = false;
+                        break;
+                }
+
+                Console.WriteLine();
+            } while (isRunning);
+        }
+    }
+}
